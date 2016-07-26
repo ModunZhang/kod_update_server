a240d7c3362b8995c95593b20822e6748b4a1c5c
DannyHe <dannyjiajia@gmail.com>
2016-07-26 16:14:38 +0800

    Debug WP:appVersion:1.1.7 appMinVersion:1.1.3 tag:2520

--- res/shaders/warning.fs
+++ res/shaders/warning.fs
@@ -8,10 +8,10 @@ varying vec2 v_texCoord;
 // 分辨率
 uniform vec2 iResolution;
 uniform float ratio;
-float RADIUS = distance(vec2(0.5), vec2(0.0));
+float RADIUS = distance(vec2(0.6), vec2(0.0));
 vec2 MIDDLE = vec2(0.5, 0.5);
-const float len = 0.4;
-const float transit = 0.2;
+const float len = 0.2;
+const float transit = 0.3;
 
 void main(void)
 {
@@ -27,23 +27,23 @@ void main(void)
 // 	float iGlobalTime = CC_Time[1];
 
 // 	float PI = 3.14159265359;
-	
+
 // 	float as = iResolution.x / iResolution.y;
-	
+
 // 	vec2 coords = gl_FragCoord.xy / iResolution.xy;
 // 	vec2 gcoords = vec2(as, 1.0)*(coords - vec2(0.5, 0.5));
-	
+
 // 	float r = 0.35*((1.0+sin(iGlobalTime))/2.0)+0.055;
 // 	float thickness = 0.08;
-	
+
 // 	float fi = mod((atan(gcoords.y, gcoords.x)-((iGlobalTime*4.0)+PI)), PI) / (2.0*PI); // 0..1
-			
+
 // 	if (abs(distance(gcoords, vec2(0.0, 0.0)) - r) < thickness*fi) {
-// 	  gl_FragColor = vec4(fi, fi, fi, 1.0);	
+// 	  gl_FragColor = vec4(fi, fi, fi, 1.0);
 // 	} else {
-// 	  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);	
+// 	  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
 // 	}
-	
+
 // }
 
 
